f78b8efb60159b95038aaaf7ec47d06ac6ce9cff
Takuya Horimatsu <new_woory@hotmail.com>
2018-05-29 23:28:28 +0900

    environment change

--- XmlValidateKt.sh
+++ XmlValidateKt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 path=$1
-echo $0
-dir=$(dirname /Users/tak/cli_validator/XmlValidateKt.sh)
+file=$0
+dir=$(dirname $0)
 java -cp ${dir}/cli_validator-1.0-SNAPSHOT.jar:. com.github.horitaku1124.cli_validator.XmlValidateKt $path
