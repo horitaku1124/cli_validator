720ec2387d5f29c6d65fb1517aa42f1567a180fe
Takuya Horimatsu <new_woory@hotmail.com>
2018-05-29 21:16:01 +0900

    first commit

---
+++ src/main/kotlin/com/github/horitaku1124/cli_validator/XmlValidate.kt
@@ -0,0 +1,43 @@
+package com.github.horitaku1124.cli_validator
+
+import org.xml.sax.InputSource
+import java.io.File
+import javax.xml.parsers.DocumentBuilderFactory
+
+
+fun main(args: Array<String>) {
+  if (args.isEmpty()) {
+    System.exit(1);
+  }
+  val path = args[0]
+  val parentDir = File(path)
+  if (parentDir.isDirectory) {
+    val files = parentDir.listFiles()
+    var succeed = true
+    for (child in files) {
+      val xmlPath = child.absoluteFile.toPath().toString()
+      val result = xmlFileCanOpen(xmlPath)
+      if (!result) {
+        succeed = false
+      }
+      println(xmlPath + " " + (if (result) "OK" else "NG"))
+    }
+    System.exit(if (succeed) 0 else 1)
+  }
+}
+
+private fun xmlFileCanOpen(xmlPath: String): Boolean {
+  return try {
+    val factory = DocumentBuilderFactory.newInstance()
+
+    factory.isIgnoringComments = true
+    factory.isIgnoringElementContentWhitespace = true
+    factory.isValidating = false
+
+    val builder = factory.newDocumentBuilder()
+    builder.parse(InputSource(xmlPath)) != null
+  } catch (e: Exception) {
+    e.printStackTrace()
+    false
+  }
+}
